d6c5fed75619e01287a3c0cadc9d5039afedcf99
FELIPE BESA <lfbesa@uc.cl>
2018-11-29 20:33:12 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :contacts
-          resources :events
+      resources :articles
+      resources :events
     end
   end
 end
