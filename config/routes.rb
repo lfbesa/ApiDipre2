d92e427e005b362c59760405f1ad37f238be90ef
lfbesa <lfbesa@uc.cl>
2019-01-14 00:31:35 -0300

    add privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :contacts
   resources :events
   root to: "home#index"
+  get '/privacy_policy', to: 'home#privacy_policy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1, defaults: { format: :json } do
